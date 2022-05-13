76cc622a4799f6d21c029862e18d97595b4665ab
hope <hope@gravylab.co.kr>
2022-05-14 01:05:25 +0900

    initial commit

---
+++ src/main/java/com/icloud/jpatest/JpaSecondaryTableTestApplication.java
@@ -0,0 +1,13 @@
+package com.icloud.jpatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSecondaryTableTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSecondaryTableTestApplication.class, args);
+    }
+
+}
